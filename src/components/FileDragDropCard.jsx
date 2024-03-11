2dfbb023b2438200d0bde6f9a6e3582f20b4dcc3
sehentschel <seanhentschel09@gmail.com>
2024-03-11 15:34:06 -0400

    added dynamic label to show selected upload file & progress bar to show files being uplaoded

--- src/components/FileDragDropCard.jsx
+++ src/components/FileDragDropCard.jsx
@@ -3,12 +3,52 @@ import "../App.css";
 import React, { useState } from "react";
 import FileDropZone from "./FileDropZone";
 import Button from "react-bootstrap/Button";
+import { useEffect } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+let interval = undefined;
 
 const FileDragDropCard = ({ setFiles }) => {
   const [uploadedFiles, setUploadedFiles] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [running, setRunning] = useState(false);
+  let interval;
+
+  useEffect(() => {
+    handleReset();
+  }, [uploadedFiles]);
+
+  useEffect(() => {
+    if (running) {
+      interval = setInterval(() => {
+        // Increment progress by a fixed amount in each interval
+        setProgress((prev) => {
+          const nextProgress = prev + 1;
+          if (nextProgress >= 100) {
+            // If progress reaches 100%, stop the animation
+            clearInterval(interval);
+            setRunning(false);
+            return 100;
+          }
+          return nextProgress;
+        });
+      }, 100); // Adjust interval timing as needed
+    } else {
+      clearInterval(interval);
+    }
+  }, [running]);
+
+  const handleReset = () => {
+    setProgress(0); // Reset progress to 0 when the button is clicked
+    setRunning(false); // Start progress bar animation
+  };
+
   const handleFilesDrop = (files) => {
-    console.log(files);
-    setFiles(files);
+    if (files !== null) {
+      setFiles(files);
+      setRunning(true);
+    }
+    console.log(`prog ${progress}`);
   };
 
   return (
@@ -21,14 +61,30 @@ const FileDragDropCard = ({ setFiles }) => {
     >
       <FileDropZone onFilesDrop={setUploadedFiles} />
       <div style={{ textAlign: "center" }}>
+        <div>
+          {uploadedFiles !== null ? (
+            <label className="skyhopText skyhopLabel">
+              Uploaded File: {uploadedFiles[0]?.name}
+            </label>
+          ) : (
+            <div></div>
+          )}
+        </div>
         <Button
           variant="primary"
-          onClick={() => handleFilesDrop(uploadedFiles)}
+          onClick={() => {
+            handleFilesDrop(uploadedFiles);
+          }}
           className="buttonModal"
           style={{ width: "50%", position: "center" }}
         >
           Upload Manifest
         </Button>
+        <ProgressBar
+          variant="info"
+          now={progress}
+          style={{ marginTop: "8px" }}
+        ></ProgressBar>
       </div>
     </div>
   );
